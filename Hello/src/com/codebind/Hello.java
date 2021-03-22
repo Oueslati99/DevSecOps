dcf6de362d4a859644f955d372c9c4c33bd3fdcd
oueslati <oueslati@ubuntu>
2021-03-22 06:39:24 -0700

    This is my first commit!

--- Hello/src/com/codebind/Hello.java
+++ Hello/src/com/codebind/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-System.out.println("Hello Karim From Montplaisir city :) !");
+System.out.println("Hello Karim From Montplaisir :) !");
 	}
 
 }
